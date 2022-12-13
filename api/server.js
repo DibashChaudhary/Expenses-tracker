2f271d86a4fbf25096dfc0317bbb8df8b34bc482
DibashChaudhary <111819870+DibashChaudhary@users.noreply.github.com>
2022-12-13 13:15:55 +1100

    ready to deploy

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
+
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import  path  from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,6 +15,10 @@ app.use(morgan("dev")); // logs all the incoming req information
 // app.use(helmet()) // setting default security headers to protect some attacks
 app.use(cors()) // allow cross origin resources
 app.use(express.json()); // convert income data in the req.body
+
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build")))
 
 // MongoDB connect
 import { connectDB } from "./src/config/dbConfig.js";
@@ -24,6 +32,15 @@ import { isAuth } from "./src/middleware/AuthMiddleware.js";
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/transaction", isAuth, transactionRouter)
 
+app.get("/dashboard", (req, res) => {
+  res.redirect("/")
+})
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/build/index.js"))
+})
+
+
 app.use("*", (req, res, next) => {
   const error = {
     message: "404 page not found",
@@ -35,15 +52,15 @@ app.use("*", (req, res, next) => {
 
 // global error handler
 app.use((error, req, res, next) => {
-  const code = error.code || 500;
-  res.status(code).json({
+  // const code = error.code || 500;
+  res.json({
     status: "error",
     message: error.message,
   });
 });
 
 app.listen(PORT, (error) => {
-
+ 
   error
     ? console.log(error)
     : console.log(`you server is ready at http://localhost:${PORT}`);
